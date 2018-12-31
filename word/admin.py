484e9077e3d8cadae57ef310c8e5933d1ff57b8a
Artem Kononov <artem.kononov@playrix.com>
2018-12-31 17:12:22 +0300

    clear all

--- word/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from word.models import Languages, Words, Translate, Dictionaries
-
-admin.site.register(Dictionaries)
-admin.site.register(Translate)
-admin.site.register(Words)
-admin.site.register(Languages)
--- word/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WordConfig(AppConfig):
-    name = 'word'
