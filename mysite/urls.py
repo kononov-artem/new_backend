cffb58e92663afc7303b24c3e4537fd5caf075d8
Artem Kononov <artem.kononov@playrix.com>
2018-12-31 22:31:57 +0300

    add rest

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('word.urls')),
+    path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
