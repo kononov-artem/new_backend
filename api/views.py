944da83c9c532ebc57c97754d4246a4dc12b225a
Artem Kononov <artem.kononov@playrix.com>
2019-01-02 17:21:32 +0300

    cleaning

--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User, Group
 from django.http import Http404
 from rest_framework import viewsets, permissions
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -13,17 +12,11 @@ from word.models import Languages, Words, Translate, Dictionaries
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserListSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = Group.objects.all()
     serializer_class = GroupListSerializer
 
