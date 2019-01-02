74907a8877c069dea73d38afca48c8a1f11dc205
Artem Kononov <artem.kononov@playrix.com>
2019-01-02 17:18:34 +0300

    cleaning project

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'', include('word.urls')),
     path(r'api/v1/', include('api.urls')),
     path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path(r'auth/', include('djoser.urls')),
--- word/urls.py
+++ word/urls.py
@@ -5,9 +5,4 @@ from word.views.list_of_dicts_by_user import get_list_of_dicts_by_user
 
 
 urlpatterns = [
-    # url(r'^dictionaries/json/', save_data, name='word_dictionaries_json'),
-    url(r'^dictionaries/get/', get_dictionary_by_name, name='word_dictionaries_get'),
-    url(r'^dictionaries/list/', get_list_of_dicts_by_user, name='word_dictionaries_list_of_dicts'),
-    # url(r'^dictionaries/traine/getWord', word_traine_get_word, name='word_traine_get_word'),
-    # url(r'^dictionaries/traine/check', word_traine_check_word, name='word_traine_check'),
 ]
